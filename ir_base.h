a3604e0bd91a2751bedbf07df931dd3c15d6aa99
Scott Mansell <phiren@gmail.com>
2019-10-01 15:17:23 +1300

    Wip: transform to IR

---
+++ ir_base.h
@@ -0,0 +1,139 @@
+#include "types.h"
+#include <optional>
+#include <type_traits>
+
+
+enum Opcode {
+    Add, // A + B
+    Sub, // A - B
+    And, // A & B
+    Or,  // A | B
+    Xor, // A ^ B
+    Shift, // A << b OR A >> -b
+
+    Cat, // A << sizeof(B) | B
+
+    load48, // base, offset, cycle
+    load32, // base, offset, cycle
+    load16, // base, offset, cycle
+    load8,  // base, offset, cycle
+    storearg, // base, offset -- To pack 4 args into store.
+    store48, // storearg, data, cycle
+    store32, // storearg, data, cycle
+    store16, // storearg, data, cycle
+    store8,  // storearg, data, cycle
+
+    ternary, // condition, true, false
+
+    Const48 = 0x8000,
+    Const,
+};
+
+struct ssa {
+    u16 offset;
+};
+
+struct IR_Base {
+    union {
+        struct {
+            u64 id : 16;
+            u64 arg_1 : 16;
+            u64 arg_2 : 16;
+            u64 arg_3 : 16;
+        };
+        struct {
+            u64 _pad1     : 16;
+            u64 num_bits  : 8;
+            u64 is_signed : 8; // bool
+            u64 arg_32    : 32;
+        };
+        struct
+        {
+            u64 _pad3  : 16;
+            u64 arg_48 : 48;
+        };
+
+        u64 hex;
+    };
+
+    IR_Base(u16 id) : id(id) { arg_48 = 0xffffffffffff; }
+    IR_Base(u16 id, ssa a) : IR_Base(id) { arg_1 = a.offset; }
+    IR_Base(u16 id, ssa a, ssa b) : IR_Base(id, a) { arg_2 = b.offset; }
+    IR_Base(u16 id, ssa a, ssa b, ssa c) : IR_Base(id, a, b) { arg_3 = c.offset; }
+    IR_Base(u16 id, u8 bits, bool _is_signed, u32 a) : IR_Base(id) { arg_32 = a; num_bits = bits; is_signed = (u8)_is_signed; }
+    IR_Base(u16 id, u64 a) : IR_Base(id) { arg_48 = a; }
+
+    template<typename T>
+    bool is() {
+        T& ret = static_cast<T>(this);
+        return ret.is();
+    }
+
+    template<typename T>
+    std::optional<T&> cast() {
+        T& ret = static_cast<T>(this);
+        if (ret.is()) {
+            return { ret };
+        }
+        return {};
+    }
+};
+
+static_assert(sizeof(IR_Base) == sizeof(u64));
+
+template<Opcode op>
+struct IR0 : public IR_Base {
+    IR0() : IR_Base(op) {}
+    bool is() const { return id == op; }
+};
+
+template<Opcode op>
+struct IR1 : public IR_Base {
+    IR1(ssa a) : IR_Base(op, a) {}
+    bool is() const { return id == op; }
+};
+
+template<Opcode op>
+struct IR2 : public IR_Base {
+    IR2(ssa a, ssa b) : IR_Base(op, a, b) {}
+    bool is() const { return id == op; }
+};
+
+template<Opcode op>
+struct IR3 : public IR_Base {
+    IR3(ssa a, ssa b, ssa c) : IR_Base(op, a, b, c) {}
+    bool is() const { return id == op; }
+};
+
+template<u8 num_bits, bool is_signed>
+struct IR_Const : public IR_Base {
+    const Opcode op = Opcode::Const;
+    template<typename T>
+    IR_Const(T i) : IR_Base(op, num_bits, is_signed, u32(i)) { static_assert(sizeof(T) <= sizeof(u32)); }
+    bool is() const { return id == op; }
+};
+
+using IR_Add = IR2<Opcode::Add>;
+using IR_Sub = IR2<Opcode::Sub>;
+using IR_And = IR2<Opcode::And>;
+using IR_Or  = IR2<Opcode::Or>;
+using IR_Xor = IR2<Opcode::Xor>;
+using IR_Cat = IR2<Opcode::Cat>;
+using IR_Ternary = IR3<Opcode::ternary>;
+using IR_Shift   = IR2<Opcode::Shift>;
+using IR_Const8  = IR_Const<8,  false>;
+using IR_Const16 = IR_Const<16, false>;
+using IR_Const24 = IR_Const<24, false>;
+using IR_Const32 = IR_Const<32, false>;
+using IR_Load8 = IR3<Opcode::load8>;
+
+
+static_assert(sizeof(IR_Add) == sizeof(IR_Base));
+
+/*struct IR_Add : public IR_Base {
+    const Opcode op = Opcode::Add;
+
+    IR_Add(u16 a, u16 b) : IR_Base(op, a, b) {}
+
+    bool is() { return id == op; }
+};*/
--- m65816.cpp
+++ m65816.cpp
@@ -3,204 +3,195 @@
 #include <stdio.h>
 #include <array>
 #include <functional>
+#include <map>
 #include <vector>
 
+#include "ir_base.h"
 
-typedef bool (*instruction_function)(m65816 &cpu, int cycle);
-
-static void load_pc_16(m65816 &cpu, int cycle, uint16_t &dest) {
-    // Reads
-    switch (cycle) {
-    case 0:
-        dest = cpu.read_p(cpu.state.pc++);
-        return;
-    case 1:
-        dest |= cpu.read_p(cpu.state.pc++) << 8;
-        return;
-    }
-}
-
-// template <int start_cycle>
-// static void write_16(m65816 &cpu, int cycleuint16_t &src) {
-//     // Reads
-//     switch (cycle) {
-//     case start_cycle:
-//         cpu.write_z(cpu.state.pc++);
-//         return;
-//     case start_cycle + 1:
-//         cpu.write_z(cpu.state.pc++) << 8;
-//         return;
-//     }
-// }
-
-
-// Absolute ADC, AND, BIT, CMP, CPX, CPY, EOR, LDA, LDX, LDY, ORA, SBC, STA, STX, STZ, STZ
-template<instruction_function fn>
-static bool timing_1a(m65816 &cpu, int cycle) {
-    switch (cycle)
-    {
-    case 2:
-    case 3:
-        load_pc_16(cpu, cycle-2, cpu.state.aa);
-        return false;
-        //return cpu.state.p.x | cpu.state.p.m; // Finish early if in 8bit mode
-    default:
-        return fn(cpu, cycle - 4);
-    }
-}
-
-// Absolute JMP (4C)
-static bool timing_1b(m65816 &cpu, int cycle) {
-    switch (cycle)
-    {
-    case 2:
-    case 3:
-        load_pc_16(cpu, cycle-2, cpu.state.aa);
-        return false;
-    case 4:
-        cpu.state.pc = cpu.state.aa;
-        return true;
-    }
-}
-
-// Absolute JSR
-static bool timing_1c(m65816 &cpu, int cycle) {
-    switch (cycle)
-    {
-    case 2:
-    case 3:
-        load_pc_16(cpu, cycle-2, cpu.state.aa);
-        return false;
-    case 4:
-        // internal operation
-        cpu.read_d(cpu.state.pc - 1); // dummy read
-        return false;
-    case 5:
-        cpu.write_z(cpu.state.s--, cpu.state.pc >> 8);
-        return false;
-    case 6:
-        cpu.write_z(cpu.state.s--, cpu.state.pc & 0xff);
-        cpu.state.pc = cpu.state.aa;
-        return true;
-    }
-}
-
-// Absolute Read-Modify-write
-// ASL, DEC, INC, LSR, ROL, ROR, TRB, TSB
-static bool timing_1d(m65816 &cpu, int cycle) {
-    switch (cycle)
-    {
-    case 2:
-    case 3:
-        load_pc_16(cpu, cycle - 2, cpu.state.aa);
-        return false;
-    case 4:
-        return true;
-    }
-}
-
-template<bool (*fn)(m65816, int)>
-// template<std::function<bool (m65816 cpu, int cycle)>>
-//template<LiteralFn<bool(m65816, int), 128> &fn>
-static void Execute(m65816 &cpu) {
-    int cycle = 2;
-    while (!fn(cpu, cycle))
-        cycle++;
-}
-
-static void StepIRLoad(m65816 &cpu);
-
-template<LiteralFn<bool(m65816, int), 128> &fn>
-static void Step(m65816 &cpu) {
-    bool end = fn(cpu, cpu.state.cycle);
-    if (end)
-        cpu.state.stepfn = StepIRLoad;
-}
-
-
-//template<bool fn(m65816 &cpu, int cycle)>
-//constexpr static int cycles(LiteralFn<bool(m65816, int), 128> &fn) {
-constexpr static int cycles(bool (*fn)(m65816, int)) {
-    int cycle = 2;
-    m65816 cpu = {};
-    cpu.state.p.x = 0;
-    cpu.state.p.m = 0;
-
-    while (!fn(cpu, cycle))
-        cycle++;
-
-    return cycle;
-}
-
-static bool ora_fn(m65816 &cpu, int cycle) {
-    if (cycle == 0) {
-        cpu.state.acc.a(cpu.read_d(cpu.state.aa++)) ;
-        return cpu.state.p.x | cpu.state.p.m;
-        cpu.state.acc.b(cpu.read_d(cpu.state.aa) | cpu.state.acc.a()) ;
-    }
-    return true;
-}
-
-//template bool timing_1a<ora_fn>(m65816 &cpu, int cycle);
-
-struct Instruction {
-    const char* name;
-    void (*execute)(m65816 &cpu); // Function that executes full instruciton
-    //void (*step)(m65816 &cpu); // Fuction that only executes a single cycle
-    //std::function<void (m65816 &cpu)> execute;
-    //LiteralFn<void (m65816 &cpu)> execute;
-    int cycle_count;
-
-    constexpr Instruction() : name(""), cycle_count(0), execute(nullptr) {};
-
-    //Instruction(char *name, LiteralFn<bool(m65816, int), 128> &fn)
-    template<bool (*fn)(m65816, int)>
-    constexpr Instruction(char *name)
-    : name(name),
-      cycle_count(cycles(fn)) {
-        //void (*e_fn)(m65816& cpu) = (void (*)(m65816 &cpu))
-        execute = Execute<fn>;
-        //step = Step<fn>;
-
-      }
+enum Reg {
+    A,
+    B,
+    C,
+    X,
+    Y,
+    PC,
+    PBR,
+    DBR,
+    Flag_M,
+    Flag_X,
+    CYCLE, // Not a register, but lets pretend.
 };
 
-using ReadFn = uint8_t(*)();
-using WriteFn = void(*)(uint8_t);
+class Emitter {
+    std::vector<IR_Base> buffer;
+    //ssa tail() { return { u16(buffer.size() - 1)}; }
 
-template<bool (*fn)(m65816, int)>
-bool AbsoluteA(m65816, int) {
+    ssa push(IR_Base&& ir) {
+        buffer.push_back(std::move(ir));
+        return { u16(buffer.size() - 1) };
+    }
+    ssa bus_a;
+
+public:
+    std::map<Reg, ssa> state;
+
+    template<u8 bits>
+    ssa Const(u32 a) {
+        return push(IR_Const<bits, false>(a));
+    }
+
+    ssa IncPC() {
+        return state[PC] = Add(state[PC], 1);
+    }
+    ssa IncCycle() {
+        return state[CYCLE] = Add(state[CYCLE], 1);
+    }
+    ssa Shift(ssa a, ssa b) {
+        return push(IR_Shift(a, b));
+    }
+    ssa Shift(ssa a, int b) {
+        return Shift(a, push(IR_Const32(b)));
+    }
+    ssa And(ssa a, ssa b) {
+        return push(IR_And(a, b));
+    }
+    ssa Or(ssa a, ssa b) {
+        return push(IR_Or(a, b));
+    }
+    ssa Xor(ssa a, ssa b) {
+        return push(IR_Xor(a, b));
+    }
+    ssa Cat(ssa a, ssa b) {
+        return push(IR_Cat(a, b));
+    }
+    ssa Zext16(ssa a) { return a; } // FIXME
+    ssa Zext32(ssa a) { return a; } // FIXME
+    ssa Read(ssa addr) {
+        return push(IR_Load8(bus_a, addr, state[CYCLE]));
+    }
+    ssa Add(ssa a, ssa b) {
+        return push(IR_Add(a, b));
+    }
+    ssa Add(ssa a, int b) {
+        return Add(a, push(IR_Const32(b)));
+    }
+    ssa Ternary(ssa cond, ssa a, ssa b) {
+        return push(IR_Ternary(cond, a, b));
+    }
+    void If(ssa cond, std::function<void()> then) {
+        std::map<Reg, ssa> old_state = state; // Copy state
+
+        then();
+
+        // Scan for differences in old and new state
+        for(auto & [key, new_val]: state) {
+            auto &old_val = old_state[key];
+            if (old_val.offset != new_val.offset) {
+                // Insert a ternary operation where the state differs.
+                new_val = Ternary(cond, old_state[key], new_val);
+            }
+        }
+    }
+};
+
+static ssa ReadPc(Emitter& e) {
+    ssa data = e.Read(e.Cat(e.state[PBR], e.state[PC]));
+    e.IncPC();
+    e.IncCycle();
+    return data;
+}
+
+static ssa ReadPc16(Emitter& e) {
+    ssa data_low = ReadPc(e);
+    ssa data_high = ReadPc(e);
+    ssa data = e.Cat(data_high, data_low);
+    return data;
+}
+
+using inner_fn = std::function<void(Emitter&, ssa&, ssa)>;
+
+static void ApplyMemoryOperation(Emitter& e, inner_fn operation, ssa address) {
+    operation(e, e.state[A], address);
+    e.IncCycle();
+
+    e.If(e.state[Flag_M], [&] () {
+        ssa address2 = e.Add(address, 1);
+        operation(e, e.state[B], address2);
+        e.IncCycle();
+    });
+}
+
+static void AbsoluteA(Emitter& e, inner_fn operation) {
+    ssa address = e.Cat(e.state[DBR], ReadPc16(e));
+    ApplyMemoryOperation(e, operation, address);
 
 }
 
-void
+ssa GetIndexReg(Emitter& e, Reg reg) {
+    ssa index = e.state[reg];
+    ssa masked_index;
+    e.If(e.state[Flag_X], [&] {
+        masked_index = e.And(index, e.Const<16>(0x00FF));
+        e.IncCycle(); // Takes an extra cycle when index is 16bit
+    });
 
-const std::array<Instruction, 256> table = []() constexpr -> std::array<Instruction, 256>  {
-    struct InstructionDef {
-        const char* name;
-        uint8_t op_base;
-        //LiteralFn<void (State, ReadFn, WriteFn), 128> fn;
-        void (*fn)(State, ReadFn, WriteFn);
+    return e.Ternary(e.state[Flag_X], masked_index, index);
+}
+
+void AbsoluteA_X(Emitter& e, inner_fn operation) {
+    ssa address = e.Cat(e.state[DBR], ReadPc16(e));
+    ssa indexed_addr = e.Add(address, GetIndexReg(e, X));
+    ApplyMemoryOperation(e, operation, indexed_addr);
+}
+
+std::array<std::function<void(Emitter&)>, 256> gen_table;
+std::array<std::string, 256> name_table;
+
+void populate_tables() {
+    auto insert = [&] (size_t opcode, std::string name, std::function<void(Emitter&)>&& fn) {
+        gen_table[opcode] = std::move(fn);
+        name_table[opcode] = name;
     };
 
-    std::array<InstructionDef, 8> universal = {
-        { "ORA", 0x00, [] (State state, uint8_t &a, ReadFn readfn, WriteFn writefn) { a |= readfn(); } },
-        { "AND", 0x20, [] (State state, uint8_t &a, ReadFn readfn, WriteFn writefn) { a &= readfn(); } },
-        { "EOR", 0x40, [] (State state, uint8_t &a, ReadFn readfn, WriteFn writefn) { a ^= readfn(); } },
-        { "ADC", 0x60, [] (State state, uint8_t &a, ReadFn readfn, WriteFn writefn) { uint16_t sum = a + readfn() + state.p.c;
-                                                                         a = sum & 0xff; state.p.c = !!(sum & 0x100); } },
-        { "STA", 0x80, [] (State state, uint8_t &a, ReadFn readfn, WriteFn writefn) { writefn(a); } },
-        { "LDA", 0xa0, [] (State state, uint8_t &a, ReadFn readfn, WriteFn writefn) { a = readfn(); } },
-        //{ "CMP", 0xc0, [] (State state, uint8_t &a, ReadFn readfn, WriteFn writefn) { compare(state.p, a, readfn()); } },
-        //{ "SBC", 0xe0, [] (State state, uint8_t &a, ReadFn readfn, WriteFn writefn) { substract_a(state, readfn()); },
+    auto universal = [&] (const char* name, size_t op_base, inner_fn fn) {
+        auto apply = [&] (size_t subop, std::function<void(Emitter& , inner_fn)> address_fn) {
+            insert(op_base + subop, name, std::bind(address_fn, std::placeholders::_1, fn));
+        };
+
+        apply(0x0d, AbsoluteA);        // a
+        apply(0x1d, AbsoluteA_X);      // a,x
+        // T[def.op_base + 0x19] = Instruction(def.name, AbsoluteA_Y<def.fn>);      // a, y
+        // T[def.op_base + 0x0f] = Instruction(def.name, Absolute_LongA<def.fn>);   // al
+        // T[def.op_base + 0x1f] = Instruction(def.name, Absolute_LongA_X<def.fn>); // al,x
+        // T[def.op_base + 0x05] = Instruction(def.name, Direct<def.fn>);           // d
+        // T[def.op_base + 0x03] = Instruction(def.name, StackRelative<def.fn>);    // d,s
+        // T[def.op_base + 0x15] = Instruction(def.name, DirectIndexedY<def.fn>);   // d,x
+        // T[def.op_base + 0x12] = Instruction(def.name, DirectIndirect<def.fn>);     // (d)
+        // T[def.op_base + 0x07] = Instruction(def.name, DirectIndirectLong<def.fn>); // [d]
+        // T[def.op_base + 0x13] = Instruction(def.name, StackRelativeIndirectIndexed<def.fn>); // (d,s),y
+        // T[def.op_base + 0x01] = Instruction(def.name, DirectIndexedIndirect<def.fn>);     // (d,x)
+        // T[def.op_base + 0x11] = Instruction(def.name, DirectIndirectIndexed<def.fn>);     // (d),y
+        // T[def.op_base + 0x17] = Instruction(def.name, DirectIndirectLongIndexed<def.fn>); // [d],y
+        // if (def.op_base != 0x80) {
+        //     T[def.op_base + 0x09] = Instruction(def.name, Immediate<def.fn>);    // #
+        // }
     };
 
-    std::array<Instruction, 256> T = {};
-    for (auto &def : universal )
+    universal("ORA", 0x00, [] (Emitter& e, ssa& reg, ssa addr) { reg = e.Or(reg, e.Read(addr)); });
+    universal("AND", 0x20, [] (Emitter& e, ssa& reg, ssa addr) { reg = e.And(reg, e.Read(addr)); });
+    universal("EOR", 0x40, [] (Emitter& e, ssa& reg, ssa addr) { reg = e.Xor(reg, e.Read(addr)); });
+    //universal("ADC", 0x60, [] (Emitter& e, ssa& reg, ssa addr) { uint16_t sum = a + readfn() + state.p.c;
+    //                                                                   a = sum & 0xff; state.p.c = !!(sum & 0x100); } },
+    //universal("STA", 0x80, [] (Emitter& e, ssa& reg, ssa addr) { writefn(a); } },
+    universal("LDA", 0xa0, [] (Emitter& e, ssa& reg, ssa addr) { reg = e.Read(addr); });
+    //universal("CMP", 0xc0, [] (Emitter& e, ssa& reg, ssa addr) { compare(state.p, a, readfn()); } },
+    //universal("SBC", 0xe0, [] (Emitter& e, ssa& reg, ssa addr) { substract_a(state, readfn()); },
+    //};
+
+    //for (auto &def : universal )
     {
-        bool (*aa)(m65816, int) = AbsoluteA<def.fn>;
-        T[def.op_base + 0x0d] = Instruction<aa>(def.name);        // a
-        // T[def.op_base + 0x1d] = Instruction(def.name, AbsoluteA_X<def.fn>);      // a,x
+
+       // apply(0x1d, AbsoluteA_X);      // a,x
         // T[def.op_base + 0x19] = Instruction(def.name, AbsoluteA_Y<def.fn>);      // a, y
         // T[def.op_base + 0x0f] = Instruction(def.name, Absolute_LongA<def.fn>);   // al
         // T[def.op_base + 0x1f] = Instruction(def.name, Absolute_LongA_X<def.fn>); // al,x
@@ -217,14 +208,12 @@ const std::array<Instruction, 256> table = []() constexpr -> std::array<Instruct
         //     T[def.op_base + 0x09] = Instruction(def.name, Immediate<def.fn>);    // #
         // }
     }
+}
 
-    return T;
-}();
 
 
 
 
 void m65816::run_for(int cycles) {
     printf("test\n");
-    printf("%s, %i\n", table[0x0d].name, table[0x0d].cycle_count);
 }
